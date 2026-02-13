3d0b2bd91096ca09124bb1dfbd02fb2b18b727eb
bobbyonmagic <bobbyonmagic@bobbyiliev.com>
2026-02-13 14:56:09 +0000

    Remove old comments

--- components/games/scalable-sentry.tsx
+++ components/games/scalable-sentry.tsx
@@ -1125,8 +1125,6 @@ export default function ScalableSentry() {
         : "bg-gradient-to-br from-gray-50 to-blue-50 text-gray-900"
     )}>
      <div className="container mx-auto max-w-4xl">
-       {/* Breadcrumb */}
-
        {/* Mobile Warning */}
        {isMobile && (
           <div className={cn(
